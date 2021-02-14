994dc82b5596ae4b35eb3ada0a31c45980649424
owen-duncan-snobel <oadster@cse.yorku.ca>
2021-02-13 20:41:46 -0500

    image_frame updated to render the images passed to it from the data prop

--- frontend/src/components/image_frame.js
+++ frontend/src/components/image_frame.js
@@ -2,24 +2,33 @@ import React, { useState, useEffect } from 'react';
 import { fabric } from 'fabric';
 
 const ImageFrame = ({ data }) => {
-	const [canvas, setCanvas] = useState('');
 	const [canvases, setCanvases] = useState([]);
-	const [numArr, setNumArr] = useState([]);
 
 	useEffect(() => {
+		// * If the data has not been fetched yet do not render a canvas (-1 icon_id refers to missing id)
 		if (data.icons[0].icon_id !== -1) {
 			data.icons.map((icon) =>
-				setCanvases([...canvases, initCanvas(icon.icon_id)])
+				setCanvases([...canvases, initCanvas(icon, icon.icon_id)])
 			);
 		}
 	}, [data]);
 
-	const initCanvas = (id) =>
-		new fabric.Canvas(id.toString(), {
+	console.log(data);
+	const initCanvas = (icon, id) => {
+		// * hardcodes the location of the image, for the final version you can use ( .download_url for the full version unscaled)
+		let imgLink = icon.raster_sizes[7].formats[0].preview_url;
+		let canvas = new fabric.Canvas(id.toString(), {
 			height: 300,
 			width: 300,
 			backgroundColor: 'white',
 		});
+
+		fabric.Image.fromURL(imgLink, function (oImg) {
+			canvas.add(oImg);
+		});
+
+		return canvas;
+	};
 
 	return (
 		<div>
